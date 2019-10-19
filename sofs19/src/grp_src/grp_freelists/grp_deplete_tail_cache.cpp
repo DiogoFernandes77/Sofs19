701396db9b8a884c57539ba896f36c65d01f7055
Pedro Alves <pedroalves99@ua.pt>
2019-10-19 20:58:03 +0100

    função deplete feita, falta testar

--- sofs19/src/grp_src/grp_freelists/grp_deplete_tail_cache.cpp
+++ sofs19/src/grp_src/grp_freelists/grp_deplete_tail_cache.cpp
@@ -1,4 +1,5 @@
 /*
+/*
  *  \author António Rui Borges - 2012-2015
  *  \authur Artur Pereira - 2016-2019
  */
@@ -22,9 +23,49 @@ namespace sofs19
     void grpDepleteTailCache(void)
     {
         soProbe(444, "%s()\n", __FUNCTION__);
+        soOpenSuperBlock ( );
+        SOSuperBlock* sb = soGetSuperBlockPointer ();
+        uint32_t nblock = sb->tail_blk; // numero do ref data block
+        uint32_t idx_ref = sb->tail_idx; // 1ª pos livre no ref data bloco
+        uint32_t ref[RPB];
+        uint32_t nfree = RPB-idx_ref;
+        soReadDataBlock(nblock, ref); // le do bloco para ref
 
+        if(nfree == 0){ // se tiver cheio cria outro bloco
+            uint32_t num = soAllocDataBlock();
+            for(int i = 0; i<RPB; i++){
+                ref[i] = NullReference;
+            }
+            soWriteDataBlock(num,ref);
+        }
+
+        else{
+
+            if(nfree >= TAIL_CACHE_SIZE){
+                for(int i=0; i<TAIL_CACHE_SIZE; i++){
+                    ref[idx_ref] = sb->tail_cache.ref[i]; // copia para a ref os valores da tail cache
+                    idx_ref++;
+                    sb->tail_cache.ref[i] = NullReference; // liberta tail cache
+                }
+                soWriteDataBlock(nblock,ref); // escreve na tail reference data block
+            }
+
+            else{
+                uint32_t k = HEAD_CACHE_SIZE - 1; 
+                for (int i = 0; i < nfree; i++)
+                {
+                    ref[idx_ref] = sb->tail_cache.ref[i]; // copia para a ref os valores da tail cache
+                    idx_ref++;
+                    sb->tail_cache.ref[k--] = NullReference;  //liberta tail cache
+                }
+                soWriteDataBlock(nblock,ref); // escreve no bloco até á última posiçao   
+            }
+        }
+
+        soSaveSuperBlock();
         /* change the following line by your code */
         binDepleteTailCache();
     }
-};
+}
+
 
