03ef3632e68ee42dba7081ff1ec80a6059decef6
Dário Alves <dario.alv@ua.pt>
2019-11-13 00:47:03 +0000

    freelists: alloc_inode, free_inode feitos e testados

--- sofs19/src/grp_src/grp_freelists/grp_alloc_inode.cpp
+++ sofs19/src/grp_src/grp_freelists/grp_alloc_inode.cpp
@@ -28,7 +28,56 @@ namespace sofs19
         soProbe(401, "%s(0x%x, 0%03o)\n", __FUNCTION__, type, perm);
 
         /* change the following line by your code */
-        return binAllocInode(type, perm);
+        
+        // "type must represent either a file (S_IFREG), a directory (S_IFDIR), or a symbolic link (S_IFLNK); if not, error EINVAL is thrown;"
+        if(type!=S_IFREG && type!=S_IFDIR && type!=S_IFLNK)
+            throw SOException(EINVAL, __FUNCTION__);
+        
+        // "perm must represent a valid permission pattern (a octal value in the range 0000 to 0777); if not, error EINVAL is thrown;"
+        if(perm<0 || perm>0777)
+        	throw SOException(EINVAL, __FUNCTION__);
+
+        // aceder superblock
+        SOSuperBlock *superblock = soGetSuperBlockPointer();
+
+        // "if there are no free inodes, error ENOSPC is thrown;"
+        if(superblock->ifree == 0)
+        	throw SOException(ENOSPC, __FUNCTION__);
+
+        // obter Inode handler
+        int inumber = superblock->ihead;
+       	int ihandler = soOpenInode(inumber);
+       	// obter Inode Pointer
+       	SOInode *inode = soGetInodePointer(ihandler);
+
+       	// superblock: atualizar head, tail e num de inodes
+       	if(superblock->ihead == superblock->itail) {
+       		superblock->itail=NullReference;
+       	}
+       	superblock->ihead = inode->next;
+       	superblock->ifree--;
+       	// guardar alteraçãos ao superblock
+       	soSaveSuperBlock();
+
+       	// Mudar info do inode (voltei a colocar alguns destes a 0 por redundância, mas possivelmente é desnecessário porque o inode já era livre)
+       	inode->mode = type | perm;
+        inode->lnkcnt = 0;
+        inode->owner = 1000;
+        inode->group = 1000;
+        inode->size = 0;
+        inode->blkcnt = 0;
+        time_t system_time;
+        time(&system_time);
+        uint32_t time = system_time;
+        inode->ctime=time;
+        inode->mtime=time;
+        inode->atime=time;
+        // guardar alterações ao inode
+        soSaveInode(ihandler);
+        // fechar inode
+        soCloseInode(ihandler);
+        return inumber;
+        // return binAllocInode(type, perm);
     }
 };
 
