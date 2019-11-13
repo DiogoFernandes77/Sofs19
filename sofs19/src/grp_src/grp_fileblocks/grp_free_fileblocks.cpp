b3e18846732ea67a7cbdd1418d1d278837a7b636
Dário Alves <dario.alv@ua.pt>
2019-11-13 11:00:26 +0000

    FreeFileBlocks: referências indiretas feitas e testadas; faltam duplas indiretas

--- sofs19/src/grp_src/grp_fileblocks/grp_free_fileblocks.cpp
+++ sofs19/src/grp_src/grp_fileblocks/grp_free_fileblocks.cpp
@@ -11,7 +11,6 @@
 
 namespace sofs19
 {
-#if false
     /* free all blocks between positions ffbn and RPB - 1
      * existing in the block of references given by i1.
      * Return true if, after the operation, all references become NullReference.
@@ -25,7 +24,6 @@ namespace sofs19
      * It assumes i2 is valid.
      */
     static bool grpFreeDoubleIndirectFileBlocks(SOInode * ip, uint32_t i2, uint32_t ffbn);
-#endif
 
     /* ********************************************************* */
 
@@ -33,21 +31,77 @@ namespace sofs19
     {
         soProbe(303, "%s(%d, %u)\n", __FUNCTION__, ih, ffbn);
 
-        /* change the following line by your code */
-        binFreeFileBlocks(ih, ffbn);
+        // aceder ao inode -> "Assumes ih is a valid handler of an inode in use opened"
+        SOInode *inode = soGetInodePointer(ih);
+        
+        // libertar File Blocks em d
+        for(; ffbn<N_DIRECT; ffbn++) {
+            if (inode->d[ffbn] != NullReference) {
+                soFreeDataBlock(inode->d[ffbn]);
+                inode->d[ffbn] = NullReference;
+                inode->blkcnt--;
+            }
+        }
+        // atualizar número de ffbn
+        ffbn -= N_DIRECT;
+        printf("FFBN: %lu\n", ffbn);
+        
+        // libertar File Blocks em i1
+        uint32_t j=0;
+        bool empty;
+        //for(j=0; j<N_INDIRECT; j++) {
+            if(inode->i1[j] != NullReference) {
+                empty = grpFreeIndirectFileBlocks(inode, inode->i1[j], ffbn);
+                // libertar i[i] caso necessário
+                if(empty) {
+                    inode->i1[j] = NullReference;
+                    inode->blkcnt--;
+                }
+                // atualizar número de ffbn
+                if (ffbn > RPB) {
+                    ffbn -= RPB;
+                } else {
+                    ffbn = 0;
+                }
+                printf("FFBN: %lu\n", ffbn);
+            }
+        //}
+        // binFreeFileBlocks(ih, ffbn);
     }
 
     /* ********************************************************* */
 
-#if false
     static bool grpFreeIndirectFileBlocks(SOInode * ip, uint32_t i1, uint32_t ffbn)
     {
         soProbe(303, "%s(..., %u, %u)\n", __FUNCTION__, i1, ffbn);
+        
+        // array de referências a file blocks
+        uint32_t ref[RPB];
+        // ler as referências do bloco
+        soReadDataBlock(i1, ref);
+        
+        uint32_t reverse = ffbn+1;
 
-        /* change the following line by your code */
-        throw SOException(ENOSYS, __FUNCTION__); 
+        for(; ffbn<RPB; ffbn++) {
+            if(ref[ffbn] != NullReference) {
+                printf("Ref indireta[0]: %lu\n", ref[ffbn]);
+                soFreeDataBlock(ref[ffbn]);
+                ref[ffbn] = NullReference;
+                ip->blkcnt--;
+            }
+        }
+        // escrever novo buffer    
+        soWriteDataBlock(i1, ref);
+        
+        for(; reverse>0; reverse--) {
+            if(ref[reverse-1] != NullReference) {
+                return false;
+            }
+        }        
+        return true;
+
+        //throw SOException(ENOSYS, __FUNCTION__); 
     }
-#endif
 
     /* ********************************************************* */
 
